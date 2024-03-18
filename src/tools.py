6e969a3d4cd810b46f7e8e8c1c16160a38ea8dfb
annprzy <anneprzybylowska@gmail.com>
2024-03-18 21:52:40 +0100

    add computing consensus motifs

--- src/tools.py
+++ src/tools.py
@@ -1,12 +1,14 @@
 from abc import ABC
+from collections import Counter
 
 import numpy as np
+import stumpy
 
 from structs import FShape
 
 
 class PromisingMotifsFinder:
-    def __init__(self, motif: FShape, threshold: float = 0.8) -> None:
+    def __init__(self, motif: FShape, threshold: float = 1.0) -> None:
         self.motif = motif
         self.threshold = threshold
 
@@ -14,8 +16,9 @@ class PromisingMotifsFinder:
         promising_motifs = []
         for i in range(len(sequence) - length + 1):
             subsequence = sequence.fshape[i:i + length]
-            if not np.isnan(subsequence).any() and (subsequence >= 1).any():
-                promising_motifs.append(FShape(subsequence, sequence.acid[i:i + length]))
+            subsequence_acid = sequence.acid[i:i + length]
+            if not np.isnan(subsequence).any() and (subsequence >= self.threshold).any():
+                promising_motifs.append(FShape(subsequence, subsequence_acid))
         return promising_motifs
 
     def find_all(self, sequence: FShape) -> dict[int, list[FShape]]:
@@ -44,6 +47,29 @@ class AgglomerativeClusterer(IClusterer):
         from sklearn.cluster import AgglomerativeClustering
 
         self.clusterer = AgglomerativeClustering(n_clusters=None, distance_threshold=eps)
+
+
+class ConsensusMotifs:
+
+    def _compute_largest_clusters(self, clusters: dict[int, list[int]]) -> list[tuple]:
+        largest = []
+        for key, cluster in clusters.items():
+            cluster_freq = Counter(cluster).most_common(3)
+            for c in cluster_freq:
+                largest.append((key, c[0], c[1]))
+        largest = sorted(largest, key=lambda x: x[2], reverse=True)[:3]
+        largest = [i for i in largest if i[2] >= 3]
+        return largest
+
+    def compute_consensus(self, motifs: dict[int, list[FShape]], clusters: dict[int, list[int]]) -> list[FShape]:
+        consensus_motifs = []
+        largest_clusters = self._compute_largest_clusters(clusters)
+        for cluster in largest_clusters:
+            cluster_motifs = [m for i, m in enumerate(motifs[cluster[0]]) if clusters[cluster[0]][i] == cluster[1]]
+            consensus = stumpy.ostinato([m.fshape for m in cluster_motifs], m=cluster[0])
+            consensus = cluster_motifs[consensus[1]]
+            consensus_motifs.append(consensus)
+        return consensus_motifs
 
 
 if __name__ == "__main__":
