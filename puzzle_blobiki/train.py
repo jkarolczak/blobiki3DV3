3a25cd7334aabcb4ad61a9c3aad32caf26412170
Jacek Karolczak <jacek.karolczak@ncodex.io>
2024-04-13 15:45:40 +0200

    Log experiments in wandb

--- puzzle_blobiki/train.py
+++ puzzle_blobiki/train.py
@@ -1,5 +1,6 @@
 import click
 import torch
+import wandb
 from numpy import mean, sqrt
 from tqdm import tqdm
 
@@ -18,8 +19,9 @@ from src.model import BlobNet
 @click.option("--num-transformers", default=8, help="Number of transformers")
 @click.option("--num-lstms", default=64, help="Number of LSTMs")
 @click.option("--dropout", default=0.0, help="Dropout")
+@click.option("--log", "log", flag_value=True, default=False, help="Log into wandb")
 def main(strategy: str, device: str, batch_size: int, num_epochs: int, learning_rate: float, hidden_size: int,
-         num_transformers: int, num_lstms: int, dropout: float) -> None:
+         num_transformers: int, num_lstms: int, dropout: float, log: bool) -> None:
     data = Data(strategy=Strategy[strategy], device=device)
     model = BlobNet(hidden_size=hidden_size, num_transformers=num_transformers, num_lstms=num_lstms, batch_size=batch_size,
                     dropout=dropout).to(torch.device(device))
@@ -29,9 +31,22 @@ def main(strategy: str, device: str, batch_size: int, num_epochs: int, learning_
     print(f"Train: {len(data.train)} instances, mean: {data.train.mean: 2.4f}, std: {data.train.std: 2.4f}")
     print(f"Valid: {len(data.valid)} instances, mean: {data.valid.mean: 2.4f}, std: {data.valid.std: 2.4f}")
     print(f"Test: {len(data.test)} instances, mean: {data.test.mean: 2.4f}, std: {data.test.std: 2.4f}")
+    not log or wandb.init(
+        project="rna-puzzles",
+        config={
+            "strategy": strategy, "device": device, "batch_size": batch_size, "num_epochs": num_epochs,
+            "learning_rate": learning_rate, "hidden_size": hidden_size, "num_transformers": num_transformers,
+            "num_lstms": num_lstms, "dropout": dropout,
+            "train_size": len(data.train), "train_mean": data.train.mean, "train_std": data.train.std,
+            "valid_size": len(data.valid), "valid_mean": data.valid.mean, "valid_std": data.valid.std,
+            "test_size": len(data.test), "test_mean": data.test.mean, "test_std": data.test.std
+        }
+    )
+
     for epoch in range(num_epochs):
-        print("=" * 30)
-        print(f"Epoch {epoch + 1}")
+        if not log:
+            print("=" * 30)
+            print(f"Epoch {epoch + 1}")
         # Train
         model.train()
         loss_history = []
@@ -43,6 +58,7 @@ def main(strategy: str, device: str, batch_size: int, num_epochs: int, learning_
             optimizer.step()
             loss_history.append(sqrt(loss.item()))
         print(f"\tTrain RMSE: {mean(loss_history): 2.4f}")
+        not log or wandb.log({"train_rmse": mean(loss_history)}, step=epoch)
 
         # Valid
         model.eval()
@@ -53,6 +69,7 @@ def main(strategy: str, device: str, batch_size: int, num_epochs: int, learning_
                 loss = criterion(y_hat, y)
                 loss_history.append(sqrt(loss.item()))
             print(f"\tValid RMSE: {mean(loss_history): 2.4f}")
+            not log or wandb.log({"valid_rmse": mean(loss_history)}, step=epoch)
 
     # Test
     loss_history = []
@@ -61,6 +78,8 @@ def main(strategy: str, device: str, batch_size: int, num_epochs: int, learning_
         loss = criterion(y_hat, y)
         loss_history.append(sqrt(loss.item()))
     print(f"Final test RMSE: {mean(loss_history): 2.4f}")
+    not log or wandb.log({"test_rmse": mean(loss_history)})
+    wandb.finish()
 
 
 if __name__ == "__main__":
