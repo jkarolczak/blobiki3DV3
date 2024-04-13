342a389dea7876e494fed73c57b38f83008559c7
Jacek Karolczak <jacek.karolczak@ncodex.io>
2024-04-13 20:23:13 +0200

    Change model

--- puzzle_blobiki/src/model.py
+++ puzzle_blobiki/src/model.py
@@ -3,48 +3,51 @@ import torch.nn as nn
 import torch.nn.functional as f
 
 
-class FeatureExtractor(nn.Module):
-    def __init__(self, input_size: int = 7, hidden_size: int = 128, num_lstms: int = 64, batch_size: int = 32,
-                 dropout: float = 0.0) -> None:
+class FeatureExtractorLayer(nn.Module):
+    def __init__(self, input_size: int = 7, output_size: int = 128) -> None:
         super().__init__()
-        self.lstm_0 = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_lstms, dropout=dropout,
-                              batch_first=True, bidirectional=True)
-        self.h0_0 = torch.randn(2 * num_lstms, batch_size, hidden_size)
-        self.c0_0 = torch.randn(2 * num_lstms, batch_size, hidden_size)
 
-        self.linear_0 = nn.Linear(2 * hidden_size, 2 * hidden_size)
+        self.weight_1 = nn.Parameter(torch.empty(input_size, output_size))
+        self.bias_1 = nn.Parameter(torch.zeros(output_size))
+        nn.init.normal_(self.weight_1)
 
-        self.lstm_1 = nn.LSTM(input_size=2 * hidden_size, hidden_size=hidden_size, num_layers=num_lstms, dropout=dropout,
-                              batch_first=True, bidirectional=True)
-        self.h0_1 = torch.randn(num_lstms, batch_size, hidden_size)
-        self.c0_1 = torch.randn(num_lstms, batch_size, hidden_size)
+        self.weight_2 = nn.Parameter(torch.empty(output_size, output_size))
+        self.bias_2 = nn.Parameter(torch.zeros(output_size))
+        nn.init.normal_(self.weight_2)
 
-        self.linear_1 = nn.Linear(2 * hidden_size, 2 * hidden_size)
+        self.layer_norm = nn.LayerNorm(output_size)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        self.h0_0, self.c0_0 = self.h0_0.to(x.device), self.c0_0.to(x.device)
-        x, (self.h0_0, self.c0_0) = self.lstm_0(x)
-        x = self.linear_0(x)
+        x = torch.matmul(x, self.weight_1) + self.bias_1
         x = f.relu(x)
-
-        self.h0_1, self.c0_1 = self.h0_1.to(x.device), self.c0_1.to(x.device)
-        x1, (self.h0_0, self.c0_0) = self.lstm_1(x)
-        x1 = self.linear_1(x1)
+        x1 = torch.matmul(x, self.weight_2) + self.bias_2
         x1 = f.relu(x1)
+        x = self.layer_norm(x1 + x)
+        return x
 
-        x = x1
+
+class FeatureExtractor(nn.Module):
+    def __init__(self, input_size: int = 7, hidden_size: int = 128, num_extractors: int = 4) -> None:
+        super().__init__()
+        self.extractor_1 = FeatureExtractorLayer(input_size=input_size, output_size=hidden_size)
+        self.deep_extractors = nn.Sequential(
+            *[FeatureExtractorLayer(input_size=hidden_size, output_size=hidden_size) for _ in
+              range(num_extractors - 1)])
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.extractor_1(x)
+        x = self.deep_extractors(x)
         x = x.max(dim=1).values
-
         return x
 
 
 class TransformerLayer(nn.Module):
     def __init__(self, hidden_size: int = 128, dropout: float = 0.0) -> None:
         super().__init__()
-        self.attention = nn.MultiheadAttention(embed_dim=2 * hidden_size, num_heads=8, dropout=dropout)
-        self.layer_norm_1 = nn.LayerNorm(2 * hidden_size)
-        self.linear = nn.Linear(2 * hidden_size, 2 * hidden_size)
-        self.layer_norm_2 = nn.LayerNorm(2 * hidden_size)
+        self.attention = nn.MultiheadAttention(embed_dim=hidden_size, num_heads=8, dropout=dropout)
+        self.layer_norm_1 = nn.LayerNorm(hidden_size)
+        self.linear = nn.Linear(hidden_size, hidden_size)
+        self.layer_norm_2 = nn.LayerNorm(hidden_size)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x1, _ = self.attention(x, x, x)
@@ -71,25 +74,22 @@ class Transformer(nn.Module):
 class RegressionHead(nn.Module):
     def __init__(self, hidden_size: int = 128) -> None:
         super().__init__()
-        self.linear_1 = nn.Linear(2 * hidden_size, hidden_size)
+        self.linear_1 = nn.Linear(hidden_size, hidden_size)
         self.linear_2 = nn.Linear(hidden_size, 1)
-        self.linear_3 = nn.Linear(1, 1)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.linear_1(x)
         x = f.relu(x)
         x = self.linear_2(x)
-        x = f.relu(x)
-        x = self.linear_3(x)
         return x
 
 
 class BlobNet(nn.Module):
-    def __init__(self, input_size: int = 7, hidden_size: int = 128, num_lstms: int = 64, num_transformers: int = 8,
-                 batch_size: int = 32, dropout: float = 0.0) -> None:
+    def __init__(self, input_size: int = 7, hidden_size: int = 128, num_extractors: int = 64, num_transformers: int = 8,
+                 dropout: float = 0.0) -> None:
         super().__init__()
-        self.feature_extractor = FeatureExtractor(input_size=input_size, hidden_size=hidden_size, num_lstms=num_lstms,
-                                                  batch_size=batch_size, dropout=dropout)
+        self.feature_extractor = FeatureExtractor(input_size=input_size, hidden_size=hidden_size,
+                                                  num_extractors=num_extractors)
         self.transformer = Transformer(num_transformers=num_transformers, hidden_size=hidden_size, dropout=dropout)
         self.head = RegressionHead(hidden_size=hidden_size)
 
--- puzzle_blobiki/train.py
+++ puzzle_blobiki/train.py
@@ -14,16 +14,16 @@ from src.model import BlobNet
 @click.option("--device", default="cpu", help="Device")
 @click.option("--batch-size", default=32, help="Batch size")
 @click.option("--num-epochs", default=10, help="Number of epochs")
-@click.option("--learning-rate", type=float, default=1e-3, help="Learning rate")
+@click.option("--learning-rate", type=float, default=1e-2, help="Learning rate")
 @click.option("--hidden-size", default=128, help="Hidden size")
 @click.option("--num-transformers", default=8, help="Number of transformers")
-@click.option("--num-lstms", default=64, help="Number of LSTMs")
+@click.option("--num-extractors", default=4, help="Number of feature extraction layers")
 @click.option("--dropout", default=0.0, help="Dropout")
 @click.option("--log", "log", flag_value=True, default=False, help="Log into wandb")
 def main(strategy: str, device: str, batch_size: int, num_epochs: int, learning_rate: float, hidden_size: int,
-         num_transformers: int, num_lstms: int, dropout: float, log: bool) -> None:
+         num_transformers: int, num_extractors: int, dropout: float, log: bool) -> None:
     data = Data(strategy=Strategy[strategy], device=device)
-    model = BlobNet(hidden_size=hidden_size, num_transformers=num_transformers, num_lstms=num_lstms, batch_size=batch_size,
+    model = BlobNet(hidden_size=hidden_size, num_transformers=num_transformers, num_extractors=num_extractors,
                     dropout=dropout).to(torch.device(device))
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     criterion = torch.nn.MSELoss()
@@ -31,12 +31,13 @@ def main(strategy: str, device: str, batch_size: int, num_epochs: int, learning_
     print(f"Train: {len(data.train)} instances, mean: {data.train.mean: 2.4f}, std: {data.train.std: 2.4f}")
     print(f"Valid: {len(data.valid)} instances, mean: {data.valid.mean: 2.4f}, std: {data.valid.std: 2.4f}")
     print(f"Test: {len(data.test)} instances, mean: {data.test.mean: 2.4f}, std: {data.test.std: 2.4f}")
+    print("=" * 30)
     not log or wandb.init(
         project="rna-puzzles",
         config={
             "strategy": strategy, "device": device, "batch_size": batch_size, "num_epochs": num_epochs,
             "learning_rate": learning_rate, "hidden_size": hidden_size, "num_transformers": num_transformers,
-            "num_lstms": num_lstms, "dropout": dropout,
+            "num_extractors": num_extractors, "dropout": dropout,
             "train_size": len(data.train), "train_mean": data.train.mean, "train_std": data.train.std,
             "valid_size": len(data.valid), "valid_mean": data.valid.mean, "valid_std": data.valid.std,
             "test_size": len(data.test), "test_mean": data.test.mean, "test_std": data.test.std
@@ -44,9 +45,8 @@ def main(strategy: str, device: str, batch_size: int, num_epochs: int, learning_
     )
 
     for epoch in range(num_epochs):
-        if not log:
-            print("=" * 30)
-            print(f"Epoch {epoch + 1}")
+        print("=" * 30)
+        print(f"Epoch {epoch + 1}")
         # Train
         model.train()
         loss_history = []
@@ -55,6 +55,7 @@ def main(strategy: str, device: str, batch_size: int, num_epochs: int, learning_
             y_hat = model(x)
             loss = criterion(y_hat, y)
             loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
             optimizer.step()
             loss_history.append(sqrt(loss.item()))
         print(f"\tTrain RMSE: {mean(loss_history): 2.4f}")
